2685a1bfeadbaccadcfd5a109b3d8e34e5f6e71b
Zweena Ariva <ariva02zweena@gmail.com>
2024-05-05 19:44:16 +0700

    React js

--- src/Components/Timeline/Posts/Post.js
+++ src/Components/Timeline/Posts/Post.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import "./Post.css"
+import { Avatar } from '@material-ui/core';
+import MoreIcon from '../../images/More.png';
+import Favorite from '../../images/favorite.png';
+import Comment from '../../images/Comment.png';
+import Send from '../../images/Send.png';
+import Bookmark from '../../images/Bookmark.png';
 
-function Post() {
+function Post({user, postImage, likes, timestamps}) {
   return (
     <div className='post'>
-        <div className=''>
-
+        <div className='post_header'>
+          <div className='post_headerAuthor'>
+            <Avatar>{user.charAt(0).toUpperCase()}</Avatar>
+            {user} <span>{timestamps}</span>
+          </div>
+          <img src={MoreIcon}/>
+        </div>
+        <div className='post_image'>
+          <img src='https://images.unsplash.com/photo-1517232115160-ff93364542dd?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+        </div>
+        <div className='post_footer'>
+          <div className='post_footerIcons'>
+            <div className='post_iconsMain'>
+              <img src={Favorite} className='postIcon'/>
+              <img src={Comment} className='postIcon'/>
+              <img src={Send} className='postIcon'/>
+            </div>
+            <div className='post_iconsSave'>
+              <img src={Bookmark} className='postIcon2'/>
+            </div>
+          </div>
+          3.700.500 Likes
         </div>
     </div>
   )
--- src/Components/Timeline/Timeline.js
+++ src/Components/Timeline/Timeline.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Timeline.css";
 import Suggestion from './Suggestion';
 import Post from './Posts/Post';
 
 function Timeline() {
+  const [posts, setPosts] = useState ([
+    {
+      user: "Zweena_",
+      postImage: "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=1769&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      likes: 100.000,
+      timestamps: "1d",
+    },
+      {
+        user: "ar07zwn",
+        postImage: "https://images.unsplash.com/photo-1550745165-9bc0b252726f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 150.000,
+        timestamps: "13h",
+      },
+      {
+        user: "lrz678",
+        postImage: "https://images.unsplash.com/photo-1506102383123-c8ef1e872756?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 30.000,
+        timestamps: "4d",
+      },
+      {
+        user: "nav0209",
+        postImage: "https://images.unsplash.com/photo-1474552226712-ac0f0961a954?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        likes: 200.000,
+        timestamps: "2h",
+      },
+  ]);
   return (
     <div className='timeline'>
         <div className='timeline_left'>
           <div className='timeline_post'>
-            <Post />
-            <Post />
-            <Post />
+           {posts.map(post => (
+              <Post user={post.user} postImage={post.postImage} like={post.likes} timestamps={post.timestamps}/>
+           ))}
           </div>
         </div>
         <div className='timeline_right'>
